b5be67e52ee8dcfefac52b1a9c48abf6cbe549ef
Kamil Górzyński <kamilgorzynski@gmail.com>
2023-03-08 12:03:50 +0100

    Z0, Z1

--- Z0/script.js
+++ Z0/script.js
@@ -1,44 +1,78 @@
 function task1() {
-    // TODO
+    const div = document.getElementById("task1");
+    div.className = "border-violet";
+    div.innerText = "Hello world!";
 }
 
 function task2() {
-    // TODO
+    const div = document.getElementById("task2");
+
+    const span = document.createElement("span");
+    span.className = "red-color";
+    span.innerText = "SPAN";
+
+    div.appendChild(span);
 }
 
 function task3() {
-    // TODO
+    const div = document.getElementById("task3");
+
+    div.innerText = "";
 }
 
 function task4() {
-    // TODO
+    const div = document.getElementById("task4");
+    const span = document.getElementById("span-to-remove");
+
+    div.removeChild(span);
 }
 
 function task5() {
-    // TODO
+    const input = document.querySelector("input[name='task5-input']");
+    console.log(input.value);
+
+    document.getElementById("task5-value").innerText = input.value;
 }
 
 function task6() {
-    // TODO
+    const div = document.getElementById("task6");
+    div.className = "green-color";
 }
 
 function task7() {
-    // TODO
+    const div = document.getElementById("task7");
+
+    const currentClass = div.className;
+
+    if (currentClass === "red-color") {
+        div.className = "green-color";
+    } else {
+        div.className = "red-color";
+    }
 }
 
 function task8() {
-    // TODO
+    const x = 8;
+    console.log("Dzień dobry " + x);
 }
 
 
 function mouseIn() {
-    // TODO
+    const div = document.getElementById("task9");
+    div.className = "background-green";
 }
 
 function mouseOut() {
-    // TODO
+    const div = document.getElementById("task9");
+    div.className = "background-red";
 }
 
 function task9() {
-    // TODO
+    const array = [1, 2, 3];
+
+    const element = array.find(function (e) {
+        return e === 2;
+    })
+
+    console.log(element);
 }
